24945c6d152e9747e0d9e9b42933f2d76aea2505
cy-bug <shashwat5590@gmail.com>
2023-03-15 21:33:07 +0530

    scss

--- src/index.ts
+++ src/index.ts
@@ -1,82 +1,58 @@
-import {
-    ViewerApp,
-    AssetManagerPlugin,
-    GBufferPlugin,
-    timeout,
-    ProgressivePlugin,
-    TonemapPlugin,
-    SSRPlugin,
-    SSAOPlugin,
-    DiamondPlugin,
-    FrameFadePlugin,
-    GLTFAnimationPlugin,
-    GroundPlugin,
-    BloomPlugin,
-    TemporalAAPlugin,
-    AnisotropyPlugin,
-    GammaCorrectionPlugin,
+import "./styles.scss";
 
-    addBasePlugins,
-    ITexture, TweakpaneUiPlugin, AssetManagerBasicPopupPlugin, CanvasSnipperPlugin,
+// import {
+// 	ViewerApp,
+// 	AssetManagerPlugin,
+// 	TonemapPlugin,
+// 	SSRPlugin,
+// 	SSAOPlugin,
+// 	BloomPlugin,
+// 	AssetManagerBasicPopupPlugin,
+// 	CanvasSnipperPlugin,
+// } from "webgi";
 
-    IViewerPlugin,
+// async function setupViewer() {
+// 	// Initialize the viewer
+// 	const viewer = new ViewerApp({
+// 		canvas: document.getElementById("webgi-canvas") as HTMLCanvasElement,
+// 		useRgbm: false,
+// 	});
 
-    // Color, // Import THREE.js internals
-    // Texture, // Import THREE.js internals
-} from "webgi";
-import "./styles.css";
+    
+// 	// Add some plugins
+// 	const manager = await viewer.addPlugin(AssetManagerPlugin);
 
-async function setupViewer(){
+// 	// Add plugins individually.
+// 	// await viewer.addPlugin(GBufferPlugin)
+// 	// await viewer.addPlugin(new ProgressivePlugin(32))
+// 	await viewer.addPlugin(new TonemapPlugin(!viewer.useRgbm));
+// 	// await viewer.addPlugin(GammaCorrectionPlugin)
+// 	await viewer.addPlugin(SSRPlugin);
+// 	await viewer.addPlugin(SSAOPlugin);
+// 	// await viewer.addPlugin(DiamondPlugin)
+// 	// await viewer.addPlugin(FrameFadePlugin)
+// 	// await viewer.addPlugin(GLTFAnimationPlugin)
+// 	// await viewer.addPlugin(GroundPlugin)
+// 	await viewer.addPlugin(BloomPlugin);
+// 	// await viewer.addPlugin(TemporalAAPlugin)
+// 	// await viewer.addPlugin(AnisotropyPlugin)
 
-    // Initialize the viewer
-    const viewer = new ViewerApp({
-        canvas: document.getElementById('webgi-canvas') as HTMLCanvasElement,
-    })
+// 	// or use this to add all main ones at once.
 
-    // Add some plugins
-    const manager = await viewer.addPlugin(AssetManagerPlugin)
+// 	// Add more plugins not available in base, like CanvasSnipperPlugin which has helpers to download an image of the canvas.
+// 	await viewer.addPlugin(CanvasSnipperPlugin);
 
-    // Add a popup(in HTML) with download progress when any asset is downloading.
-    await viewer.addPlugin(AssetManagerBasicPopupPlugin)
+// 	// This must be called once after all plugins are added.
+// 	viewer.renderer.refreshPipeline();
 
-    // Add plugins individually.
-    // await viewer.addPlugin(GBufferPlugin)
-    // await viewer.addPlugin(new ProgressivePlugin(32))
-    // await viewer.addPlugin(new TonemapPlugin(!viewer.useRgbm))
-    // await viewer.addPlugin(GammaCorrectionPlugin)
-    // await viewer.addPlugin(SSRPlugin)
-    // await viewer.addPlugin(SSAOPlugin)
-    // await viewer.addPlugin(DiamondPlugin)
-    // await viewer.addPlugin(FrameFadePlugin)
-    // await viewer.addPlugin(GLTFAnimationPlugin)
-    // await viewer.addPlugin(GroundPlugin)
-    // await viewer.addPlugin(BloomPlugin)
-    // await viewer.addPlugin(TemporalAAPlugin)
-    // await viewer.addPlugin(AnisotropyPlugin)
+// 	await manager.addFromPath("./assets/classic-watch.glb");
 
-    // or use this to add all main ones at once.
-    await addBasePlugins(viewer)
+// 	// Load an environment map if not set in the glb file
+// 	// await viewer.scene.setEnvironment(
+// 	//     await manager.importer!.importSinglePath<ITexture>(
+// 	//         "./assets/environment.hdr"
+// 	//     )
+// 	// );
+// }
 
-    // Add more plugins not available in base, like CanvasSnipperPlugin which has helpers to download an image of the canvas.
-    await viewer.addPlugin(CanvasSnipperPlugin)
-
-    // This must be called once after all plugins are added.
-    viewer.renderer.refreshPipeline()
-
-    await manager.addFromPath("./assets/classic-watch.glb")
-
-    // Load an environment map if not set in the glb file
-    // await viewer.scene.setEnvironment(
-    //     await manager.importer!.importSinglePath<ITexture>(
-    //         "./assets/environment.hdr"
-    //     )
-    // );
-
-    // Add some UI for tweak and testing.
-    const uiPlugin = await viewer.addPlugin(TweakpaneUiPlugin)
-    // Add plugins to the UI to see their settings.
-    uiPlugin.setupPlugins<IViewerPlugin>(TonemapPlugin, CanvasSnipperPlugin)
-
-}
-
-setupViewer()
+// setupViewer();
