bce47ab07f0948aa2c2e79db165deb889d9d5092
cy-bug <shashwat5590@gmail.com>
2023-03-16 18:26:55 +0530

    send help

---
+++ components/header/Header.tsx
@@ -0,0 +1,9 @@
+import styles from "./Header.module.scss";
+
+export default function Header() {
+	return (
+		<div className={styles.header}>
+			<h1 className={styles.headerTitle}>Header</h1>
+		</div>
+	);
+}
