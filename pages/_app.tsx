cc139024016fb04a73b2b1c321f28177a6d7fe57
cy-bug <shashwat5590@gmail.com>
2023-03-20 15:30:42 +0530

    more app changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Header from "@/components/header/Header";
-import Lenis from "@studio-freight/lenis";
 import React from "react";
 import Cursor from "@/components/cursor/Cursor";
 
 export default function App({ Component, pageProps }: AppProps) {
 	React.useEffect(() => {
 		const scroll = document.getElementById("scroll");
-		const lenis = new Lenis();
-		function raf(time: any) {
-			lenis.raf(time);
-			requestAnimationFrame(raf);
-		}
-		requestAnimationFrame(raf);
-
 		window.addEventListener("scroll", () => {
 			let scrollPerc =
 				(window.scrollY /
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,24 +3,50 @@ import Head from "next/head";
 import { gsap } from "gsap";
 import ScrollTrigger from "gsap/dist/ScrollTrigger";
 import { useEffect, useRef } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import Lenis from "@studio-freight/lenis";
 
 function HomePage() {
 	gsap.registerPlugin(ScrollTrigger);
 	const titleRef = useRef<HTMLDivElement>(null);
 
 	useEffect(() => {
+		const lenis = new Lenis();
+		function raf(time: any) {
+			lenis.raf(time);
+			requestAnimationFrame(raf);
+		}
+		requestAnimationFrame(raf);
+
 		const title = titleRef.current;
 		if (title) {
 			let tl = gsap.timeline({
 				scrollTrigger: {
 					trigger: title,
-					start: "top 40%",
+					start: "top 30%",
 					end: "bottom 30%",
 					scrub: true,
 				},
 			});
-			tl.to(title, {opacity: 0});
+			tl.to(title, { opacity: 0 });
 		}
+
+		const pages = document.querySelectorAll("#pageTrigger");
+		pages.forEach((page, index) => {
+			ScrollTrigger.create({
+				trigger: page,
+				start: "top 70%",
+				onEnter: () => {},
+			});
+		});
+
+		const beachImg = document.getElementById("beachImg");
+		beachImg?.addEventListener("mouseenter", () => {
+			gsap.to(beachImg, {
+				zoom: 1.1,
+			})
+		});
 	}, []);
 
 	return (
@@ -30,18 +56,50 @@ function HomePage() {
 			</Head>
 
 			<main>
-				<div className={styles.page}>
+				<div className={styles.page} page-index="1">
+					<div id="pageTrigger"></div>
 					<div className={styles.landing}>
 						<div className={styles.title} ref={titleRef}>
 							<span cursor-class="grow">Worldwide</span>
-							<span cursor-class="grow">Clean Oceans</span>
+							<span cursor-class="grow">Clean Water</span>
 						</div>
 					</div>
 				</div>
-				<div className={styles.page}>
-					<h2 className="pageTitle">
-						
-					</h2>
+				<div className={styles.page} page-index="2">
+					<div id="pageTrigger"></div>
+					<div className={styles.content}>
+						<div className={styles.card}>
+							<img
+								className={styles.pageImg}
+								src="/images/beach1.jpg"
+								alt="turtle swimming in ocean"
+								id="beachImg"
+								// fill
+								// priority
+								cursor-class="hide"
+							/>
+						</div>
+						<div className={styles.card}>
+							<div className={styles.inner}>
+								<h2 className={styles.pageTitle}>
+									What&apos;s this all about?
+								</h2>
+								<p className={styles.pageDesc}>
+									The global water pollution crisis is a major
+									threat for acheiving sustainable
+									development. It not only affects the
+									upcoming generation but has a drastic impact
+									on us and other beings in the environment.
+									<br />
+								</p>
+							</div>
+							{/* <div className={styles.inner}>
+								<h2 className="pageLink">
+									<Link href="/showreel">Watch Showreel</Link>
+								</h2>
+							</div> */}
+						</div>
+					</div>
 				</div>
 			</main>
 		</>
