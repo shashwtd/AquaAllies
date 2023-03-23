253d6df30d1a53a36872856cb3d324db831a0cb9
cy-bug <shashwat5590@gmail.com>
2023-03-23 13:52:10 +0530

    removing noise effect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,7 +21,7 @@ export default function App({ Component, pageProps }: AppProps) {
 			<Header />
 			<Cursor />
 			<div className="scrollBar" id="scroll"></div>
-			<div className="noise"></div>
+			{/* <div className="noise"></div> */}
 			<Component {...pageProps} />
 		</>
 	);
