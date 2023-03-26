c7a120e4dfe9a313dd02fa8825f0c5fbbb93eedc
cy-bug <shashwat5590@gmail.com>
2023-03-26 19:02:34 +0530

    implementing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,27 @@
+import React from "react";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Header from "@/components/header/Header";
-import React from "react";
 import Cursor from "@/components/cursor/Cursor";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function App({ Component, pageProps }: AppProps) {
-	React.useEffect(() => {
-		const scroll = document.getElementById("scroll");
-		window.addEventListener("scroll", () => {
-			let scrollPerc =
-				(window.scrollY /
-					(document.body.scrollHeight - window.innerHeight)) *
-				100;
-			scroll!.style.setProperty("--scroll", `${scrollPerc}%`);
-		});
-	}, []);
+  React.useEffect(() => {
+    const scroll = document.getElementById("scroll");
+    window.addEventListener("scroll", () => {
+      let scrollPerc =
+        (window.scrollY / (document.body.scrollHeight - window.innerHeight)) *
+        100;
+      scroll!.style.setProperty("--scroll", `${scrollPerc}%`);
+    });
+  }, []);
 
-	return (
-		<>
-			<Header />
-			<Cursor />
-			<div className="scrollBar" id="scroll"></div>
-			{/* <div className="noise"></div> */}
-			<Component {...pageProps} />
-		</>
-	);
+  return (
+    <AnimatePresence>
+      <Header />
+      <Cursor />
+      <div className="scrollBar" id="scroll"></div>
+      <Component {...pageProps} />
+    </AnimatePresence>
+  );
 }
---
+++ pages/reports.tsx
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+
+function Reports() {
+  return (
+    <div>
+      <h1>Reports</h1>
+    </div>
+  );
+}
+
+export default Reports;
