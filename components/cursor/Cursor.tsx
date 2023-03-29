b1639132fb598b4560659de1b06951db527dddde
cy-bug <shashwat5590@gmail.com>
2023-03-29 12:33:23 +0530

    new function to hide cursor completly

--- components/cursor/Cursor.tsx
+++ components/cursor/Cursor.tsx
@@ -1,55 +1,67 @@
-import React, { useState } from 'react';
-import styles from './Cursor.module.css';
+import React, { useState } from "react";
+import styles from "./Cursor.module.css";
+
+function ResetCursor() {
+  const cursor = document.querySelector("#cursor") as HTMLElement;
+  cursor.setAttribute("state", "");
+  cursor.removeAttribute("invisible")
+}
+
+function HideCursor() {
+  const cursor = document.querySelector("#cursor") as HTMLElement;
+  cursor.setAttribute("invisible", "");
+}
 
 const Cursor = () => {
   React.useEffect(() => {
-    const cursor = document.querySelector('#cursor') as HTMLElement;
-    const cursorCircle = document.querySelector('#cursorCircle') as HTMLElement;
+    const cursor = document.querySelector("#cursor") as HTMLElement;
+    const cursorCircle = document.querySelector("#cursorCircle") as HTMLElement;
     var wobble = true;
 
     const mouse = { x: -100, y: -100 }; // mouse pointer's coordinates
     const pos = { x: 0, y: 0 }; // cursor's coordinates
     const speed = 0.08; // between 0 and 1
 
-    const updateCoordinates = (e: { clientX: number; clientY: number; }) => {
+    const updateCoordinates = (e: { clientX: number; clientY: number }) => {
       mouse.x = e.clientX;
       mouse.y = e.clientY;
-    }
+    };
 
-    window.addEventListener('mousemove', updateCoordinates);
-    window.addEventListener('mouseout', () => { cursor.setAttribute('state', 'hide'); });
-    window.addEventListener('mouseover', () => { cursor.setAttribute('state', ''); });
+    window.addEventListener("mousemove", updateCoordinates);
+    window.addEventListener("mouseout", () => {
+      cursor.setAttribute("state", "hide");
+    });
+    window.addEventListener("mouseover", () => {
+      cursor.setAttribute("state", "");
+    });
 
     function getAngle(diffX: number, diffY: number) {
-      return Math.atan2(diffY, diffX) * 180 / Math.PI;
+      return (Math.atan2(diffY, diffX) * 180) / Math.PI;
     }
 
     function getSqueeze(diffX: number, diffY: number) {
-      const distance = Math.sqrt(
-        Math.pow(diffX, 2) + Math.pow(diffY, 2)
-      );
+      const distance = Math.sqrt(Math.pow(diffX, 2) + Math.pow(diffY, 2));
       const maxSqueeze = 0.15;
       const accelerator = 1500;
       return Math.min(distance / accelerator, maxSqueeze);
     }
 
-
     const updateCursor = () => {
       const diffX = Math.round(mouse.x - pos.x);
       const diffY = Math.round(mouse.y - pos.y);
-      
+
       pos.x += diffX * speed;
       pos.y += diffY * speed;
-      
+
       const angle = getAngle(diffX, diffY);
       const squeeze = getSqueeze(diffX, diffY);
-      
-      const scale = 'scale(' + (1 + squeeze) + ', ' + (1 - squeeze) +')';
-      const rotate = 'rotate(' + angle +'deg)';
-      const translate = 'translate3d(' + pos.x + 'px ,' + pos.y + 'px, 0)';
+
+      const scale = "scale(" + (1 + squeeze) + ", " + (1 - squeeze) + ")";
+      const rotate = "rotate(" + angle + "deg)";
+      const translate = "translate3d(" + pos.x + "px ," + pos.y + "px, 0)";
 
       cursor.style.transform = translate;
-      if (wobble) cursorCircle.style.transform = scale + ' ' + rotate;
+      if (wobble) cursorCircle.style.transform = scale + " " + rotate;
     };
 
     function loop() {
@@ -59,21 +71,20 @@ const Cursor = () => {
 
     requestAnimationFrame(loop);
 
-
-    const cursorModifiers = document.querySelectorAll('[cursor-class]');
-    cursorModifiers.forEach(cursorModifier => {
-      cursorModifier.addEventListener('mouseenter', function(this: any) {
-        const cursorClass = this.getAttribute('cursor-class');
-        cursor.setAttribute('state', cursorClass);
-        cursorClass == "arrow" ? wobble=false : wobble=true;
+    const cursorModifiers = document.querySelectorAll("[cursor-class]");
+    cursorModifiers.forEach((cursorModifier) => {
+      cursorModifier.addEventListener("mouseenter", function (this: any) {
+        const cursorClass = this.getAttribute("cursor-class");
+        cursor.setAttribute("state", cursorClass);
+        cursorClass == "arrow" ? (wobble = false) : (wobble = true);
         if (cursorClass == "arrow") {
-          cursorCircle.style.transform = 'rotate(0deg)';
+          cursorCircle.style.transform = "rotate(0deg)";
         }
       });
-      
-      cursorModifier.addEventListener('mouseleave', function() {
-        cursor.setAttribute('state', '');
-        wobble=true;
+
+      cursorModifier.addEventListener("mouseleave", function () {
+        cursor.setAttribute("state", "");
+        wobble = true;
       });
     });
   }, []);
@@ -82,7 +93,8 @@ const Cursor = () => {
     <div className={styles.cursor} id="cursor">
       <div className={styles.cursorCircle} id="cursorCircle"></div>
     </div>
-  )
-}
+  );
+};
 
 export default Cursor;
+export { ResetCursor, HideCursor };
