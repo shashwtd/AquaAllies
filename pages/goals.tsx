93ca88b94170c73e39211cdc0c81353f49e2031f
cy-bug <shashwat5590@gmail.com>
2023-04-03 18:43:15 +0530

    writing content for goals page

--- pages/goals.tsx
+++ pages/goals.tsx
@@ -1,5 +1,9 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from "react";
 import Cursor, { ResetCursor, HideCursor } from "@/components/cursor/Cursor";
+import css from "@/styles/goals.module.css";
+import Image from "next/image";
+import Carausel from "@/components/carausel/Carausel";
 
 const Goals = () => {
   return (
@@ -8,36 +12,97 @@ const Goals = () => {
 
       <main>
         {/* Generate sample article */}
-        <article className="article">
-          <div className="article__content">
-            <h1>Goals</h1>
+        <article className={css.article}>
+          <div className={css.article__content}>
+            <h1> The 2030 Agenda — Sustainable Development Goals</h1>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-              ullamcorper, diam vitae feugiat tristique, ex urna rutrum enim, id
-              efficitur est sapien at nisl. Vivamus eu nisl ac magna consectetur
-              eleifend. Ut quis semper nisi. Sed vitae ornare est, non aliquam
-              massa. Fusce eleifend, odio a tristique tincidunt, urna tortor
-              tincidunt nunc, sit amet gravida ipsum lorem vitae purus. Mauris
-              euismod, arcu et lobortis aliquam, nibh nisl faucibus metus, non
-              porttitor nisl ante nec odio. Sed metus risus, dictum quis
-              tincidunt a, lacinia at erat. Ut euismod, enim sed suscipit
-              hendrerit, risus nisi porttitor justo, nec porta quam tortor a mi.
-              Mauris gravida aliquam sem, vel placerat nunc. Nunc ac scelerisque
-              odio. Ut suscipit, sapien ac dapibus commodo, elit lorem aliquam
-              elit, sed posuere nibh nibh quis dolor. Curabitur sit amet cursus
-              enim. Proin eget ultrices libero. Sed ut lacus euismod, hendrerit
-              sapien non, malesuada tellus.
+              The Sustainable Development Goals (SDGs, also referred to as the
+              Global Goals) are the blueprint to achieve a better and more
+              sustainable future for all. The key element of the 2030 Agenda,
+              address the global challenges we face, including those related to
+              poverty, inequality, climate, environmental degradation,
+              prosperity, and peace and justice. The Goals interconnect and, to
+              ensure a just transition that leaves no one behind, we must
+              achieve each Goal and target by 2030. Each SDG is comprised of a
+              set of sub-targets. Additionally, each goal has a set of
+              indicators, jointly known as the global indicator framework, with
+              which to assess progress. The global indicator framework was
+              developed by the Inter-Agency and Expert Group on SDG Indicators
+              (IAEG-SDGs) and agreed to, as a practical starting point at the
+              47th session of the UN Statistical Commission held in March 2016.
+              The report of the Commission, which included the global indicator
+              framework, was then taken note of by ECOSOC at its 70th session in
+              June 2016. The links in the list of SDGs below will take you to
+              the Goals’ unique targets and indicators, as well as recent
+              updates on progress.
+            </p>
+            <p>By far, There are 17 Sustainable Development Goals (SDGs)</p>
+            <Carausel />
+            <br />
+            <p>
+              Now that you know about the 17 Sustainable Development Goals,
+              let's shift our focus to the 6th SDG - Clean Water and Sanitation.
+              This goal aims to ensure availability and sustainable management
+              of water and sanitation for all. Achieving this goal is crucial
+              for preventing the spread of diseases and improving the overall
+              well-being of individuals and communities. Let's explore the
+              targets and indicators of this and see how we can contribute
+              towards achieving it by 2030.
+            </p>
+            <br />
+            <hr />
+            <br />
+            <br />
+            <h1>Goal 6 — Clean Water and Sanitation</h1>
+            <p>
+              The sixth Sustainable Development Goal (SDG) in the 2030 Agenda
+              for Sustainable Development is Clean Water and Sanitation. This
+              goal is focused on ensuring access to clean water and adequate
+              sanitation facilities for all. The SDG is essential because access
+              to clean water and proper sanitation is a fundamental human right
+              and a prerequisite for achieving sustainable development.
+              According to the United Nations, nearly one-third of the world's
+              population lacks access to safe drinking water, and more than half
+              of the world's population lacks access to adequate sanitation
+              facilities. The lack of access to clean water and sanitation
+              facilities is particularly acute in developing countries, where
+              millions of people die each year from water-related diseases.
             </p>
             <p>
-              Donec ut libero sit amet ligula iaculis aliquam. Donec at lectus
-              imperdiet, dapibus nunc eu, ultricies sapien. Donec dui nulla,
-              consequat eu ipsum id, ultricies scelerisque turpis. Donec
-              scelerisque, sapien sed viverra luctus, justo enim varius enim,
-              nec imperdiet tortor ante non ante. Sed non risus vitae mi egestas
-              vehicula. Nulla facilisi. Fusce sed viverra erat, ac fringilla
-              nisl. Nulla facilisi. Nullam nec erat in nibh pulvinar aliquam ac
-              ac ante. Aenean at ligula eget nisl ultricies ultricies. Nulla
-              facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
+              The SDG aims to ensure that everyone has access to safe and
+              affordable drinking water and basic sanitation services by 2030.
+              The target is to improve water quality and increase access to
+              adequate sanitation facilities for all. This includes providing
+              access to improved water sources, such as piped water, protected
+              wells, and rainwater harvesting systems. It also includes
+              improving water quality by reducing pollution and increasing water
+              treatment facilities.
+            </p>
+            <p>
+              The goal of the Clean Water and Sanitation SDG is critical in
+              ensuring that everyone has access to basic necessities that are
+              crucial for their well-being. The achievement of this goal will
+              not only save lives but will also contribute significantly to the
+              development of societies. The provision of clean water and
+              adequate sanitation facilities will not only improve health but
+              will also provide opportunities for economic growth and increased
+              social well-being.
+            </p>
+            <br />
+            <h4>
+              "Ensuring availability and sustainable management of water and
+              sanitation for all"
+            </h4>
+            <br />
+            <p>
+              To reach universal access to drinking water, sanitation and
+              hygiene by 2030, the current rates of progress would need to
+              increase fourfold. Achieving these targets would save 829,000
+              people annually, who die from diseases directly attributable to
+              unsafe water, inadequate sanitation and poor hygiene practices.
+              This goal is to ensure availability
+              and sustainable management of water and sanitation for all. More
+              specifically, 8 targets need to be attained by 2030:
             </p>
           </div>
         </article>
