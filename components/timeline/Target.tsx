effb3d22b364cf9c2ea9d9f121d0288ae9d1c03a
cy-bug <shashwat5590@gmail.com>
2023-04-06 04:15:54 +0530

    making the timeline component

---
+++ components/timeline/Target.tsx
@@ -0,0 +1,70 @@
+import styles from "./Timeline.module.css";
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import { TextPlugin } from "gsap/dist/TextPlugin";
+
+gsap.registerPlugin(ScrollTrigger, TextPlugin);
+
+const TimelineTarget = (prop: { iter: string; desc: string }) => {
+  const timeline = useRef(null);
+  const timelineItems = useRef(null);
+
+  useEffect(() => {
+    gsap.set(timelineItems.current, { opacity: 0.7, scale: 0.9 });
+    gsap.to(timelineItems.current, {
+      borderColor: "#1113",
+      opacity: 1,
+      scale: 1,
+      ease: "power4.out",
+      scrollTrigger: {
+        trigger: timeline.current,
+        start: "top 90%",
+        end: "bottom 90%",
+        scrub: true,
+      },
+    });
+
+    gsap.set(timeline.current, { "--bg": "#8e8e8e", "--s": 0.8 });
+    gsap.to(timeline.current, {
+      "--bg": "#646464",
+      "--s": 1,
+      ease: "power4.out",
+      scrollTrigger: {
+        trigger: timeline.current,
+        start: "top 80%",
+        end: "bottom 80%",
+        scrub: true,
+        onEnter: () => {
+          gsap.to("#timelineNum span", {
+            text: prop.iter.slice(2),
+            duration: 0.5,
+            ease: "power4.out",
+          });
+        },
+        onEnterBack: () => {
+          gsap.to("#timelineNum span", {
+            text: prop.iter.slice(2),
+            duration: 0.5,
+            ease: "power4.out",
+          });
+        },
+      },
+    });
+  }, [prop.iter]);
+
+  return (
+    <div className={styles.timeline__item} ref={timeline}>
+      <div
+        className={styles.timeline__content}
+        ref={timelineItems}
+        cursor-class="grow"
+      >
+        <h2>Target {prop.iter} —</h2>
+        <p>{prop.desc}</p>
+      </div>
+    </div>
+  );
+};
+
+export default TimelineTarget;
--- components/timeline/Timeline.tsx
+++ components/timeline/Timeline.tsx
@@ -2,6 +2,7 @@ import gsap from "gsap";
 import ScrollTrigger from "gsap/dist/ScrollTrigger";
 import styles from "./Timeline.module.css";
 import { useEffect, useRef } from "react";
+import TimelineTarget from "./Target";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -14,57 +15,18 @@ const Timeline = () => {
   return (
     <div className={styles.timeline}>
       <div className={styles.timeline__container} ref={timeline}>
+        <div className={styles.num} id="timelineNum">
+          <span>1</span>
+        </div>
         <div className={styles.timeline__items} ref={timelineItems}>
-          <div className={styles.timeline__item}>
-            <div className={styles.timeline__content}>
-              <h2>2015</h2>
-              <p>
-                The United Nations General Assembly adopts the 2030 Agenda for
-                Sustainable Development, which includes the 17 Sustainable
-                Development Goals (SDGs).
-              </p>
-            </div>
-          </div>
-          <div className={styles.timeline__item}>
-            <div className={styles.timeline__content}>
-              <h2>2016</h2>
-              <p>
-                The United Nations General Assembly adopts the 2030 Agenda for
-                Sustainable Development, which includes the 17 Sustainable
-                Development Goals (SDGs).
-              </p>
-            </div>
-          </div>
-          <div className={styles.timeline__item}>
-            <div className={styles.timeline__content}>
-              <h2>2017</h2>
-              <p>
-                The United Nations General Assembly adopts the 2030 Agenda for
-                Sustainable Development, which includes the 17 Sustainable
-                Development Goals (SDGs).
-              </p>
-            </div>
-          </div>
-          <div className={styles.timeline__item}>
-            <div className={styles.timeline__content}>
-              <h2>2018</h2>
-              <p>
-                The United Nations General Assembly adopts the 2030 Agenda for
-                Sustainable Development, which includes the 17 Sustainable
-                Development Goals (SDGs).
-              </p>
-            </div>
-          </div>
-          <div className={styles.timeline__item}>
-            <div className={styles.timeline__content}>
-              <h2>2019</h2>
-              <p>
-                The United Nations General Assembly adopts the 2030 Agenda for
-                Sustainable Development, which includes the 17 Sustainable
-                Development Goals (SDGs).
-              </p>
-            </div>
-          </div>
+          <TimelineTarget iter="6.1" desc="By 2030, achieve universal and equitable access to safe and affordable drinking water for all" />
+          <TimelineTarget iter="6.2" desc="By 2030, achieve access to adequate and equitable sanitation and hygiene for all and end open defecation, paying special attention to the needs of women and girls and those in vulnerable situations" />
+          <TimelineTarget iter="6.3" desc="By 2030, improve water quality by reducing pollution, eliminating dumping and minimizing release of hazardous chemicals and materials, halving the proportion of untreated wastewater and substantially increasing recycling and safe reuse globally" />
+          <TimelineTarget iter="6.4" desc="By 2030, substantially increase water-use efficiency across all sectors and ensure sustainable withdrawals and supply of freshwater to address water scarcity and substantially reduce the number of people suffering from water scarcity" />
+          <TimelineTarget iter="6.5" desc="By 2030, implement integrated water resources management at all levels, including through transboundary cooperation as appropriate" />
+          <TimelineTarget iter="6.6" desc="By 2020, protect and restore water-related ecosystems, including mountains, forests, wetlands, rivers, aquifers and lakes" />
+          <TimelineTarget iter="6.A" desc="By 2030, expand international cooperation and capacity-building support to developing countries in water- and sanitation-related activities and programmes, including water harvesting, desalination, water efficiency, wastewater treatment, recycling and reuse technologies" />
+          <TimelineTarget iter="6.B" desc="Support and strengthen the participation of local communities in improving water and sanitation management" />
         </div>
       </div>
     </div>
