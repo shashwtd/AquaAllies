64004585bb97be125ed1e26aa78e40430d3198de
cy-bug <shashwat5590@gmail.com>
2023-04-17 15:52:45 +0530

    custom curtain direction

--- components/curtain/Curtain.tsx
+++ components/curtain/Curtain.tsx
@@ -2,9 +2,18 @@ import styles from "./Curtain.module.css";
 import React from "react";
 import { gsap } from "gsap";
 
-function ThrowCurtain(CurtainThrown: any) {
+var direction_ = "right"
+
+function ThrowCurtain(CurtainThrown: any, direction: string) {
+  direction_ = direction;
   const curtain = document.querySelector("." + styles.curtain) as HTMLElement;
-  if (curtain) gsap.set(curtain, { x: "-100vw" , scaleX: 0.3, transformOrigin: "right"});
+  if (curtain) {
+    if (direction === "right") {
+      gsap.set(curtain, { x: "-100vw", scaleX: 0.3, transformOrigin: "right" });
+    } else if (direction === "left") {
+      gsap.set(curtain, { x: "100vw", scaleX: 0.3, transformOrigin: "left" });
+    }
+  }
   gsap.to(curtain, {
     duration: 0.6,
     transform: "translateX(0)",
@@ -21,22 +30,31 @@ function ThrowCurtain(CurtainThrown: any) {
 function RemoveCurtain() {
   const curtain = document.querySelector("." + styles.curtain) as HTMLElement;
   const waiter = document.querySelector("." + styles.waiter);
-  if (waiter) gsap.set(waiter, {delay: 0.4, opacity: 0 });
+  if (waiter) gsap.set(waiter, { delay: 0.4, opacity: 0 });
+  var x = "translateX(100vw)";
+  if (direction_ === "right") x = "translateX(100vw)"
+  if (direction_ === "left") x = "translateX(-100vw)"
   gsap.to(curtain, {
     delay: 0.8,
     duration: 0.8,
     ease: "power2.in",
-    transform: "translateX(100vw)",
+    transform: x,
     scaleX: 0.2,
     onComplete: () => {
-      if (curtain) gsap.set(curtain, {x: "-100vw", scaleX: 1});
+      if (curtain) {
+        if (direction_ === "right") {
+          gsap.set(curtain, { x: "-100vw", scaleX: 1 });
+        } else if (direction_ === "left") {
+          gsap.set(curtain, { x: "100vw", scaleX: 1 });
+        }
+      }
     },
   });
 }
 
 function Curtain() {
   return (
-    <div className={styles.curtain}  onClick={RemoveCurtain}>
+    <div className={styles.curtain} onClick={RemoveCurtain}>
       <div className={styles.waiter}>
         <div className={styles.waiting}></div>
         <h2>loading..</h2>
