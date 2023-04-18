477e347db6c1f2c709842e23cc07866d14902be9
cy-bug <shashwat5590@gmail.com>
2023-04-18 08:15:43 +0530

    responsiveness

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -2,34 +2,34 @@ import styles from "./Header.module.css";
 import Pass from "@/components/pass/Pass";
 import { showPlayer } from "../player/Player";
 
-
 export default function Header() {
+  function showMenu() {
+
+  }
   return (
     <div className={styles.header}>
       <div className={styles.inner}>
         <Pass href="/">
-          <h1 className={styles.headerTitle} >
-            AquaAlly.
-          </h1>
+          <h1 className={styles.headerTitle}>AquaAlly.</h1>
         </Pass>
         <div className={styles.options}>
           <Pass href="/">
-            <span >home</span>
+            <span>home</span>
           </Pass>
           <Pass href="/goals">
-            <span >goals</span>
+            <span>goals</span>
           </Pass>
           <Pass href="/contribute">
-            <span >contribute</span>
+            <span>contribute</span>
           </Pass>
         </div>
         <div className={styles.action}>
-          <span
-            className={styles.actionButton}
-            
-            onClick={showPlayer}
-          >
+          <span className={styles.actionButton} onClick={showPlayer} btn-action="player">
             Showreel
+            <span>→</span>
+          </span>
+          <span className={styles.actionButton} onClick={showMenu} btn-action="menu">
+            Menu
             <span>→</span>
           </span>
         </div>
