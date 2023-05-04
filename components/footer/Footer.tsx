4f36f8f49ef5d44e4723fc37727e7325e8e3b6fd
cy-bug <shashwat5590@gmail.com>
2023-05-04 13:21:06 +0530

    added footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -4,6 +4,44 @@ import styles from "./Footer.module.css";
 export default function Footer(prop: { scrollTop: any }) {
   const router = useRouter();
   return (
-    <div id="daFooter" className={styles.footer} onClick={prop.scrollTop}></div>
+    <>
+      <div className={styles.lane}>
+        <div className={styles.inner}>
+          <>
+            <p className={styles.proj}>©️ {new Date().getFullYear()} AquaAllies</p>
+          </>
+          <>
+            <ul>
+              <li>
+                <a
+                  href="http://github.com/shashwtt/AquaAllies"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Project Repo
+                </a>
+              </li>
+              <li>
+                <a
+                  href="http://github.com/shashwtt"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Github
+                </a>
+              </li>
+              <li>
+                <a href="mailto:shashwat5590@gmail.com">Email</a>
+              </li>
+            </ul>
+          </>
+        </div>
+      </div>
+      <div
+        id="daFooter"
+        className={styles.footer}
+        onClick={prop.scrollTop}
+      ></div>
+    </>
   );
 }
